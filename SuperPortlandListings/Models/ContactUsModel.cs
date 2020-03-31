03dfded259ebe9e51024a454819e3ccc22a3dd57
logan <logan.testa@outlook.com>
2020-03-31 15:38:29 -0700

    Add project files.

---
+++ SuperPortlandListings/Models/ContactUsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperPortlandListings.Models
+{
+    public class ContactUsModel
+    {
+        public string userName { get; set; }
+        public string userEmail { get; set; }
+        public string userZipCode { get; set; }
+        public string userSubject { get; set; }
+        public string userMessage { get; set; }
+
+
+    }
+}
---
+++ SuperPortlandListings/Models/listing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperPortlandListings.Models
+{
+    public class listing
+    {
+        public string name { get; set; }
+        public string classCSS { get; set; }
+        public string city { get; set; }
+        public string address { get; set; }
+        public string price { get; set; }
+        public string placedOnMarket { get; set; }
+        public string shortDescription { get; set; }
+        public string description { get; set; }
+
+        public listing(string name, string classCSS, string city, string address, string price, string placedOnMarket, string shortDescription, string description)
+        {
+            this.name = name;
+            this.classCSS = classCSS;
+            this.city = city;
+            this.address = address;
+            this.price = price;
+            this.placedOnMarket = placedOnMarket;
+            this.shortDescription = shortDescription;
+            this.description = description;
+        }
+    }
+}
