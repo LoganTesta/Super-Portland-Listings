e5683ae5640b641108fa8dc41735b0f4c54e6e0a
logan <logan.testa@outlook.com>
2020-04-14 20:54:09 -0700

    Save selected value for price range

--- SuperPortlandListings/Models/ListingsModel.cs
+++ SuperPortlandListings/Models/ListingsModel.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SuperPortlandListings.Models
 {
     public class ListingsModel
     {
+        [BindProperty]
         public string searchCity { get; set; }
+        [BindProperty]
         public string searchByOptions { get; set; }
     }
 }
