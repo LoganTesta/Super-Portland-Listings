41c7bd0387c83af2ba301523bd7f6ccd9f2422ba
logan <logan.testa@outlook.com>
2020-04-21 15:29:56 -0700

    Add mobile menu close icon

--- SuperPortlandListings/wwwroot/assets/javascript/javascript-functions.js
+++ SuperPortlandListings/wwwroot/assets/javascript/javascript-functions.js
@@ -1,11 +1,13 @@
 ﻿
 $(".mobile-nav__menu").eq(0).on("click", function () {
+    $(".mobile-nav__menu").eq("0").toggleClass("show");
     $(".mobile-nav").eq("0").toggleClass("show");
     $(".body-wrapper").toggleClass("hide");
 });
 
 $(window).resize(function () {
     if ($(window).width() >= 1200) {
+        $(".mobile-nav__menu").eq("0").removeClass("show");
         $(".mobile-nav").eq("0").removeClass("show");
         $(".body-wrapper").removeClass("hide");
     }
